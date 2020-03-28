b71581a61079fd71d0153672b7da9557eb09a7e6
zenlaboratory <zenlaboratory@gmail.com>
2020-03-28 01:40:55 +0100

    3rd commit.

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,11 @@
 // Programa para calcular la letra del DNI y almacenar los datos DNI/Nombre en una variable tipo Map().
 // No se chequean los errores de introducción de datos.
 
+// Importación de las librerias necesarias pra el programa, dart:io para posibilitar la entrada y salida de datos por consola.
+
 import 'dart:io';
+
+// Declaración de las variables utilizadas en el programa.
 
 var personas = {};
 var dni = '';
@@ -10,16 +14,24 @@ var nombre = '';
 var modulo = 0;
 var letrasDni = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
+// Programa principal.
+
 void main(List<String> arguments) {
 
   do {
+
+    // Introducción del número del DNI del usuario y cálculo de la letra correspondiente al número introducido.
 
     stdout.write('Introduce tu número de DNI sin letra (0=EXIT): ');
     dni = stdin.readLineSync();
     modulo = int.parse(dni)%23;
     dniConLetra = dni + letrasDni[modulo];
 
+    // Comprobamos la opción introducida por el ususario y verificamos que no es el valor 0 para salir del programa y que el DNI no está repetido.
+
     if ((dni != '0') && (!personas.containsKey(dniConLetra))) {
+
+      // Introduccción del nombre del usuario.
 
       stdout.write('Introduce tu nombre: ');
       nombre = stdin.readLineSync();
@@ -27,7 +39,11 @@ void main(List<String> arguments) {
 
       }
 
-      else if ((dni != '0') && (personas.containsKey(dniConLetra))) { 
+      // Comprobamos si el DNI ya está registrado.
+
+      else if (personas.containsKey(dniConLetra)) { 
+
+        // En este caso el DNI ya se encuentra registrado en el sistema.
       
         print('DNI ya registrado introduzca un DNI valido... \n');
 
@@ -35,11 +51,15 @@ void main(List<String> arguments) {
 
       else {
 
+        // Salida de la condicional al introducir el valor 0.
+
         break; 
       
       }
     
   } while (dni != '0');
+
+  // Impresión del Map() con los datos introducidos.
 
   print (personas);
 
